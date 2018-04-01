db3d26460fb8cbb1a27c4c5bdec6c2a19072d1e2
NKwnf <wangningfei7@gmail.com>
2018-03-31 23:55:40 -0400

    new features

---
+++ classification/classification_nn_tensorflow.py
@@ -0,0 +1,128 @@
+#!/usr/local/bin/python
+# coding:utf-8
+
+import os
+import numpy as np
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import np_utils
+from keras.callbacks import TensorBoard
+from keras.layers import Dropout
+
+
+def get_name(filename):
+	name_list = os.listdir(filename)
+	return name_list
+
+def readfile(filename):
+	with open(filename) as file:
+		data = file.read()
+	return data
+
+def get_data(path, x, y, label):
+	name_list = get_name(path)
+	data1 = readfile(os.path.join(path, name_list[0]))
+	data1 = data1.split(",")
+	# print data
+	data1.pop()
+	ori = len(data1)
+	for each in name_list:
+		if each == ".DS_Store":
+			continue
+		data = readfile(os.path.join(path, each))
+		data = data.split(",")
+		# print data
+		data.pop()
+		l = len(data)
+		# data.pop(33)
+		for k in range(0, len(data)):
+			data[k] = float(data[k])
+			# trainx.append()
+		if l == ori:
+			x.append(data)
+			a = each.split("_")[0]
+			y.append([label[a]])
+	# print len(x)
+	# print y
+		# print data
+def main(X_train, y_train, X_test, y_test):
+	num_classes = 75
+	# for i in range(0,len(X_train)):
+	# 	for j in range(0,len(X_train[0])):
+	# 		X_train[i][j] = int(X_train[i][j])
+	# for i in range(0,len(X_test)):
+	# 	for j in range(0,len(X_test[0])):
+	# 		X_test[i][j] = int(X_test[i][j])
+	X_train = np.array(X_train)
+	X_test = np.array(X_test)
+	# y_train = np.array(y_train)
+	# y_test = np.array(y_test)
+	y_train = np_utils.to_categorical(y_train,75)
+	y_test = np_utils.to_categorical(y_test,75)
+	# one hot encode outputs
+	# y_train = np_utils.to_categorical(y_train,num_classes)
+	# y_test = np_utils.to_categorical(y_test,num_classes)
+
+	# design model
+	model = Sequential()
+	model.add(Dense(400, input_dim = 38, activation ='relu'))
+	model.add(Dropout(0.2))
+	model.add(Dense(300, input_dim = 400, activation ='relu'))
+	model.add(Dropout(0.2))
+	model.add(Dense(200, input_dim=300, activation='relu'))
+	model.add(Dropout(0.2))
+	model.add(Dense(200, input_dim=200, activation='relu'))
+	model.add(Dropout(0.2))
+	model.add(Dense(num_classes, activation = 'softmax'))
+	# tensorboard = TensorBoard(log_dir='log', histogram_freq=0, write_graph=True, write_images=True)
+
+	# print out summary of the model
+	print(model.summary())
+
+	# Compile model
+	model.compile(loss ='categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+
+	# Fit the model
+	his = model.fit(X_train, y_train, validation_data = (X_test, y_test), epochs = 20, batch_size =100, verbose = 2)
+	# Final evaluation of the model
+	# print (max(his.history['val_acc']))
+	# list_his.append(max(his.history['val_acc']))
+	# loss.append(min(his.history['val_loss']))
+	scores = model.evaluate(X_test, y_test, verbose=0)
+	print("Baseline Error: %.2f%%" % (100-scores[1]*100))
+	# print list_his
+	# print loss
+
+
+
+if __name__ == '__main__':
+	# global testx, testy, trainx,trainy
+	# global path_train, path_test
+	path_train = '/Users/ningfeiwang/Documents/spring2018/cse498_info_privacy/project/Code_De-anonymization/dataset/classification/trainingfeature'
+	path_test = '/Users/ningfeiwang/Documents/spring2018/cse498_info_privacy/project/Code_De-anonymization/dataset/classification/testingfeature'
+	testx = []
+	testy = []
+	trainx = []
+	trainy = []
+	label = dict()
+	name_list = get_name(path_train)
+	p = 0
+	for i in range(0,len(name_list)):
+		if name_list[i] == ".DS_Store":
+			continue
+		a = name_list[i].split("_")[0]
+		if a in label.keys():
+			continue
+
+		label[a] = p
+		p += 1
+	print (p)
+	# print len(label)
+	get_data(path_train, trainx, trainy, label)
+	print (len(trainx))
+	print (len(trainx[0]))
+	get_data(path_test, testx, testy, label)
+
+	main(trainx,trainy,testx,testy)
+	
--- features/ext_features.py
+++ features/ext_features.py
@@ -18,7 +18,7 @@ def readfile(filename):
 
 def writefeatures(label,features):
 	name = label.split(".")[0] + ".txt"
-	f = open(os.path.join("trainingfeature",name), 'a')
+	f = open(os.path.join("testingfeature",name), 'a')
 	for i in range(0,len(features)):
 		f.write(str(features[i]))
 		f.write(",")
@@ -135,9 +135,25 @@ def nums_lenline(name_list):
 			for j in range(0,len(line)):
 				if line[j] != " ":
 					l += 1 
-		lenline[each] = float(l/k)
+		q = float(l/k)
+		l = 0
+		k = 0
+		lk = 0
+		for i in range(0, len(lines_f)):
+			if "#" in lines_f[i]:
+				lines_f[i] = lines_f[i].split("#")[0]
+			if lines_f[i] == "":
+				continue
+			k += 1
+			line = lines_f[i].split(" ")
+			for j in range(0,len(line)):
+				if line[j] != " ":
+					l += 1
+			lk += (l - q) ** 2
+		lk = lk/k
+		lenline[each] = [float(l/k)]
 		print lenline
-		writefeatures(each, [lenline[each]])
+		writefeatures(each, lenline[each])
 
 def comment_len(name_list):
 	comment = {}
@@ -169,17 +185,72 @@ def comment_len(name_list):
 		print comment
 		writefeatures(each, comment[each])
 
-# def check_space(name_list):
+def check_space(name_list):
+	# space = dict()
+	for each in name_list:
+		if each == ".DS_Store":
+			continue
+		data = readfile(os.path.join(path,each))
+		# equ_nums = data.count("=")
+		# clean_nums = data.count("==") + data.count("!=")
+		equ_nums_space = data.count(" = ")
+		# real_equ = equ_nums - clean_nums
+		if equ_nums_space > 2:
+			# print 1
+			writefeatures(each, [float(1)])
+		else:
+			writefeatures(each, [float(0)])
 
+def par_nums(name_list):
+	# space = dict()
+	for each in name_list:
+		if each == ".DS_Store":
+			continue
+		f = open(os.path.join(path,each),'r')
+		lines_f = f.readlines()
+		f.close()
+		l = 0
+		k = 0
+		for i in range(0, len(lines_f)):
+			if "def" in lines_f[i]:
+				par_nums = re.findall(r'(.*)', lines_f[i])[0].count(",") + 1
+				l += 1
+				k += par_nums
+		if l == 0:
+			writefeatures(each, [float(0)])
+			# print 0
+		else:
+			writefeatures(each, [float(k/l)])
+			# print 1
+def empty_line(name_list):
+	for each in name_list:
+		if each == ".DS_Store":
+			continue
+		f = open(os.path.join(path,each),'r')
+		lines_f = f.readlines()
+		f.close()
+		l = 0
+		k = 0
+		for i in range(0, len(lines_f)):
+			if lines_f[i].strip() == "":
+				l += 1
+			k += 1
+		if l == 0:
+			writefeatures(each, [float(0)])
+		else:
+			writefeatures(each, [float(k/l)])
 
 
 if __name__ == '__main__':
 	global path
-	path = '/Users/ningfeiwang/Documents/spring2018/cse498_info_privacy/project/Code_De-anonymization/dataset/classification/training'
+	path = '/Users/ningfeiwang/Documents/spring2018/cse498_info_privacy/project/Code_De-anonymization/dataset/classification/testing'
 	name_list = get_name(path)
 	keywords(name_list)
 	detect_lang(name_list)
 	nums_function(name_list)
 	nums_lenline(name_list)
 	comment_len(name_list)
+	check_space(name_list)
+	par_nums(name_list)
+	empty_line(name_list)
 	
